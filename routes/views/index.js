04ea2e0e2844a1b11f69f7501e26fc8052c82a32
Kirk Hodgman <kjhodgman@gmail.com>
2018-06-26 19:04:39 -0700

    added dynamic titles and various css updates

--- routes/views/index.js
+++ routes/views/index.js
@@ -7,6 +7,8 @@ exports = module.exports = function (req, res) {
 
 	// locals.section is used to set the currently selected item in the header navigation.
 	locals.section = 'home';
+	locals.title = 'A Fistful of Vinyl'
+
 
 	view.query('hero', keystone.list('Session')
 		.model
