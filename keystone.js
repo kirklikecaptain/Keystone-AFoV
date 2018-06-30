0cada34006388193b87ad9f138c5711ca695fbb9
Kirk Hodgman <kjhodgman@gmail.com>
2018-06-30 11:02:52 -0700

    css stuff

--- keystone.js
+++ keystone.js
@@ -10,7 +10,7 @@ keystone.init({
 
 	'sass': 'public',
 	'static': 'public',
-	'static options': { maxAge: '14d' },
+	'static options': { maxAge: '5d' },
 	'favicon': 'public/favicon.ico',
 	'views': 'templates/views',
 	'view engine': '.hbs',
