5a4506b807a14d9c84d23a8001aa8067be12acc8
Kirk Hodgman <kjhodgman@gmail.com>
2018-11-01 22:05:39 -0700

    fix contact form

--- models/Enquiry.js
+++ models/Enquiry.js
@@ -10,10 +10,15 @@ Enquiry.add({
 	name: { type: Types.Name, required: true },
 	email: { type: Types.Email, required: true },
 	phone: { type: String },
+	enquiryType: { type: Types.Select, options: [
+		{ value: 'message', label: 'Just leaving a message' },
+		{ value: 'question', label: 'I\'ve got a question' },
+		{ value: 'other', label: 'Complaint or Praise' },
+	] },
 	message: { type: Types.Markdown, required: true },
 	createdAt: { type: Date, default: Date.now },
 });
 
 Enquiry.defaultSort = '-createdAt';
-Enquiry.defaultColumns = 'name, email, enquiryType, createdAt';
+Enquiry.defaultColumns = 'name, email, createdAt';
 Enquiry.register();
